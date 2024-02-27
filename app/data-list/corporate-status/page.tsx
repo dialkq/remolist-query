854acf9dfb4e4dd6c6cffbe3193edea4728ba15f
dialkq <dialkq@gmail.com>
2024-02-27 17:49:38 +0700

    using react query for table component

--- app/data-list/corporate-status/page.tsx
+++ app/data-list/corporate-status/page.tsx
@@ -2,17 +2,23 @@
 import TableList from "./TableList";
 import SearchBar from "./SearchBar";
 import { Modal } from "./Modal";
-const Page = () => {
+import TablelistQuery from "./TableListQuery";
+import { QueryClientProvider, QueryClient } from "react-query";
 
-  ``
+
+const queryClient = new QueryClient();
+const Page = () => {
+  ``;
   return (
-    <div className="w-full flex flex-col bg-white">
-      <div className="flex items-center justify-between my-3 sm:my-5 w-10/12 mx-auto">
-        <SearchBar />
-        <Modal />
+    <QueryClientProvider client={new QueryClient()}>
+      <div className="w-full flex flex-col bg-white">
+        <div className="flex items-center justify-between my-3 sm:my-5 w-10/12 mx-auto">
+          <SearchBar />
+          <Modal />
+        </div>
+        <TablelistQuery />
       </div>
-      <TableList />
-    </div>
+    </QueryClientProvider>
   );
 };
 
