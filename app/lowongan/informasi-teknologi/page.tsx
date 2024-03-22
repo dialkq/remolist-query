5995d5b0c2361557acc11d6d79532422a13fe7b4
dialkq <dialkq@gmail.com>
2024-03-22 22:35:09 +0700

    fix sidebar

--- app/lowongan/informasi-teknologi/page.tsx
+++ app/lowongan/informasi-teknologi/page.tsx
@@ -6,6 +6,7 @@ import styles from "./style.module.css";
 import { useState } from "react";
 import Lottie from "lottie-react"; // Impor pustaka Lottie
 import animation from "./animation.json";
+import Sidebar from "@/components/common/Sidebar";
 
 const lowonganList: TabsLowonganProps[] = [
   {
@@ -46,30 +47,42 @@ const Page = () => {
 
   return (
     <div className="w-full flex bg-white justify-evenly">
-      {/* Bagian TabsLowongan */}
-      <div
-        className={`flex flex-col w-11/12 md:w-1/3 mb-5 lg:my-5 gap-y-6 overflow-auto h-screen ${styles.cardTabs}`}
-      >
-        {lowonganList.map((item, index) => (
-          <TabsLowongan
-            key={index}
-            {...item}
-            onClick={() => handleJobClick(item)}
-          />
-        ))}
+      <div className="hidden lg:flex w-auto">
+        <Sidebar />
       </div>
-      <div
-        className={`w-7/12 mb-5 lg:my-5 bg-white overflow-auto rounded-3xl h-screen hidden md:flex md:flex-col ${styles.deskripsiTabs}`}
-      >
-        {activeJob ? (
-          <CardLowongan data={activeJob} />
-        ) : (
-          <div className="mx-auto my-auto">
-            <Lottie className="h-40 w-40 mx-auto" animationData={animation} />
-            <p className="font-bold text-2xl text-center">Anda belum memilih iklan lowongan kerja</p>
-            <p className="font-semibold text-slate-500 text-center text-base">Pilih iklan di sebelah kiri untuk melihat detailnya di sini.</p>
-          </div>
-        )}
+      {/* TABS AND CARDS LOWONGAN */} 
+      <div className="bg-yellow-100 flex flex-col lg:flex-row justify-evenly w-full">
+        <div className="flex lg:hidden">
+          <Sidebar />
+        </div>
+        <div
+          className={`bg-red-100 flex flex-col w-full md:w-1/3 mb-5 lg:my-5 gap-y-6 overflow-auto h-screen ${styles.cardTabs}`}
+        >
+          {lowonganList.map((item, index) => (
+            <TabsLowongan
+              key={index}
+              {...item}
+              onClick={() => handleJobClick(item)}
+            />
+          ))}
+        </div>
+        <div
+          className={`bg-blue-100 w-7/12 mb-5 lg:my-5 overflow-auto rounded-3xl h-screen hidden md:flex md:flex-col ${styles.deskripsiTabs}`}
+        >
+          {activeJob ? (
+            <CardLowongan data={activeJob} />
+          ) : (
+            <div className="mx-auto my-auto">
+              <Lottie className="h-40 w-40 mx-auto" animationData={animation} />
+              <p className="font-bold text-2xl text-center">
+                Anda belum memilih iklan lowongan kerja
+              </p>
+              <p className="font-semibold text-slate-500 text-center text-base">
+                Pilih iklan di sebelah kiri untuk melihat detailnya di sini.
+              </p>
+            </div>
+          )}
+        </div>
       </div>
     </div>
   );
