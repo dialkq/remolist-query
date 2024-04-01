1a5887b1d918cd67a0fc5eec805e0c8cdc0ad2ce
dialkq <dialkq@gmail.com>
2024-04-01 22:30:07 +0700

    install firebase

---
+++ app/authContext.ts
@@ -0,0 +1,13 @@
+// import { useContext, createContext, useState, useEffect } from "react";
+
+// const AuthContext = createContext(null);
+
+// export const AuthContextProvider = ({ children }) => {
+//     return (
+//         <AuthContext.Provider>{children}</AuthContext.Provider>
+//     )
+// }
+
+// export const UserAuth = () => {
+//     return useContext(AuthContext)
+// }
